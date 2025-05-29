49cf232936b8a4c5e7adaf502fd794ac6776b4f7
Salem Almezel <sfalmezel@outlook.com>
2025-05-29 17:26:21 -0300

    update

--- Gettingstarted-part1.py
+++ Gettingstarted-part1.py
@@ -24,6 +24,27 @@
 # part3 = input("The 3rd part: ") + '!'
 # print(part + "-" + part2 + "-" + part3)
 
-result = 10 * 25
-# the following line produces an error
-print("The result is " + str(result))
+# result = 10 * 25
+# # the following line produces an error
+# print("The result is " + str(result))
+
+
+# name = "Tim Tester"
+# age = 20
+# skill1 = "python"
+# level1 = "beginner"
+# skill2 = "java"
+# level2 = "veteran"
+# skill3 = "programming"
+# level3 = "semiprofessional"
+# lower = 2000
+# upper = 3000
+
+# print(f"my name is {name}, I am {age} years old")
+# print('')
+# print("my skills are")
+# print(f" - {skill1} ({level1})")
+# print(f" - {skill2} ({level2})")
+# print(f" - {skill3} ({level3})")
+# print('')
+# print(f"I am looking for a job with a salary of {lower}-{upper} euros per month")
