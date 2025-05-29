627aabbbb634ccb3d70a12353ad8eaf712c56005
Salem Almezel <sfalmezel@outlook.com>
2025-05-29 15:03:40 -0300

    first lesson in class

---
+++ Gettingstarted-part1.py
@@ -0,0 +1,29 @@
+#print(":-)")
+
+#print(365 * 24 * 60)
+    
+# name = input('What is your name?')
+# print(name)
+# print(name)
+
+# name = input('What is your name?')
+
+# print("!" + name + "!" + name + "!")
+
+# firstname = input("Given name:")
+# familyname = input('Family name:')
+# streetadd = input("Street address:")
+# postal = input('City and postal code:')
+# print(firstname + " " + familyname) 
+# print(streetadd)
+# print(postal)
+
+# Fix the code
+# part = input("The 1st part: ")
+# part2 = input("The 2nd part: ")
+# part3 = input("The 3rd part: ") + '!'
+# print(part + "-" + part2 + "-" + part3)
+
+result = 10 * 25
+# the following line produces an error
+print("The result is " + str(result))
