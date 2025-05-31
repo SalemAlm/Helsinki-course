ed5efaff3206d8a4cb132521289caee17d57e0fe
Salem Almezel <sfalmezel@outlook.com>
2025-05-30 22:54:29 -0300

    Part 1 complete

--- Gettingstarted-part1.py
+++ Gettingstarted-part1.py
@@ -48,3 +48,116 @@
 # print(f" - {skill3} ({level3})")
 # print('')
 # print(f"I am looking for a job with a salary of {lower}-{upper} euros per month")
+
+# # Write your solution here
+# x = 27
+# y = 15
+# addition = x + y
+# sub = x - y 
+# multiply = x * y 
+# divide = x / y 
+# print(f'{x} + {y} = {addition}')
+# print(f'{x} - {y} = {sub}')
+# print(f'{x} * {y} = {multiply}')
+# print(f'{x} / {y} = {divide}')
+
+# Fix the code
+#print(5)
+#print(" + ")
+#print(8)
+#print(" - ")
+#print(4)
+#print(" = ")
+#print(5 + 8 - 4)
+# result = 9
+# print(f'5 + 8 - 4 = {result}')
+
+
+# num_input = int(input(f'Please type in a numnber:' ))
+# multiplied = num_input * 5
+# print(f'{num_input} times 5 is {multiplied}')
+
+
+# days = int(input(f'How many days? '))
+# seconds = days*24*60*60
+# print(f'Seconds in that many days: {seconds}')
+
+# number = int(input(f'Number 1: '))
+# number_2 = int(input(f'Number 2: '))
+# number_3 = int(input(f'Number 3: '))
+# number_4 = int(input(f'Number 4: '))
+# print(f'The sum of the numbers is {number+number_2+number_3+number_4}  and the mean is {(number+number_2+number_3+number_4)/4}')
+
+# num_of_stud = int(input(f'How many students on the course?' ))
+# group = int(input(f'Desired group size? '))
+
+# if group % 2 !=0:
+#  print(f'Number of groups formed: {num_of_stud//group+1}')
+# else: 
+#  print(f'Number of groups formed: {num_of_stud/group}')
+
+# Conditional statment section; 
+
+# number = int(input(f'Please type in a number: '))
+# if number == 1984:
+#     print('Orwell')
+
+# # Write your solution here
+# number = int(input('Please type in a number: '))
+# if number < 0 : 
+#     print(f'The absolute value of this number is {number*-1}')
+# else:
+#     print(f'The absolute value of this number is {number}')
+
+# name = input('Please tell me your name: ')
+
+# if name != 'Jerry' : 
+#     soup = int(input(f'How many portions of soup? '))
+#     print(f'The total cost is {soup*5.9}')
+# print('Next please!')
+
+# number = int(input("Please type in a number: "))
+
+# # Check and print based on the number's size
+# if number < 1000:
+#     print("This number is smaller than 1000")
+#     if number < 100:
+#         print("This number is smaller than 100")
+#         if number < 10:
+#             print("This number is smaller than 10")
+
+# # Always end with this
+
+
+# Write your solution here
+
+# num_1 = int(input(f'Number 1: '))
+# num_2 = int(input(f'Number 2: '))
+# operation = input('')
+# if operation == "add" : 
+#     print(f'{num_1} + {num_2} = {num_1+num_2}')
+# elif operation == "multiply" :
+#     print(f'{num_1} * {num_2} = {num_1*num_2}')
+# elif operation == "subtract" :
+#     print(f'{num_1} - {num_2} = {num_1-num_2}')
+
+# Write your solution here
+# fah = int(input(f'Please type in a temperature (f): '))
+# cel = (fah - 32) * 5 / 9 
+# if cel < 0: 
+#     print(f'{fah} degrees Fahrenheit equals {cel} degrees Celsius')
+#     print(f"Brr! It's cold in here!")
+# elif cel >= 0: 
+#     print(f'{fah} degrees Fahrenheit equals {cel} degrees Celsius')
+
+# hourly = int(input(f'Hourly wage: '))
+# work = int(input(f'Hours worked: '))
+# Day = input('Day of the week:  ')
+# if Day == "Sunday":
+#     print(f'Daily wages: {(hourly*2) * work} euros')
+# else: 
+#     print(f'Daily wages: {hourly * work} euros')
+
+# from math import sqrt 
+
+# print(sqrt(9))
