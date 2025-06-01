e40e1f3119de4503bbbf0098b13966eafc80cef3
Salem Almezel <sfalmezel@outlook.com>
2025-06-01 02:13:35 -0300

    not done yet

---
+++ Part_2.py
@@ -0,0 +1,16 @@
+# word = input("Please type in a word: ")
+# if len(word)>1 :
+#     print(f'There are {len(word)} in the word {word}')
+#     print('Thank you!')
+# else: 
+#     print("Thank you!")
+
+# first = int(input('Please type in the first number: '))
+# second = int(input('Please type in another number: '))
+# if first > second: 
+#     print(f"The greater number was: {first}")
+# elif second>first: 
+#     print(f'The greater number was {second}')
+# else: 
+#     print('The numbers are equal!')
+
