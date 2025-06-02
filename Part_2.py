711240ef3880a0485092e83a77b5f9f440d3ee94
Salem Almezel <sfalmezel@outlook.com>
2025-06-02 01:59:47 -0300

    not done yet

--- Part_2.py
+++ Part_2.py
@@ -14,3 +14,31 @@
 # else: 
 #     print('The numbers are equal!')
 
+# Write your solution here
+# age = int(input('What is your age? '))
+# if age>=5: 
+#     print(f"Ok, you're {age} years old")
+# elif age<=4 and age>=0:
+#     print(f"I suspect you can't write quite yet...")
+# else: 
+#     print('That must be a mistake')
+
+# Write your solution here
+# name = input('Please type in your name: ')
+# if name in ["Huey", "Dewey", "Louie"]:
+#     print("I think you might be one of Donald Duck's nephews.")
+# elif name in ["Morty", "Ferdie"]:
+#     print("I think you might be one of Mickey Mouse's nephews.")
+# else: 
+#     print("You're not a nephew of any character I know of.")
+
+# Write your solution here
+# number = int(input('Number: '))
+# if number % 5 == 0 and number % 3 == 0:
+#     print('FizzBuzz')
+# elif number % 3 == 0: 
+#     print('Fizz')
+# elif number % 5 == 0:
+#     print('Buzz')
+# else: 
+#     print(f'number: {number}')
