a388bb414a5d44040aa25f3eeea98715b9972de6
Salem Almezel <sfalmezel@outlook.com>
2025-06-04 01:34:25 -0300

    started next section

---
+++ Part_3.py
@@ -0,0 +1,26 @@
+# # Write your solution here
+# number = 2
+# while 2<=number<=30:
+#     print(number)
+#     number += 2
+
+# Fix the program
+# print("Are you ready?")
+# number = int(input("Please type in a number: "))
+# while number > 0 :
+#     print(number)
+#     number -= 1
+
+# print("Now!")
+    
+#    upper = int(input('Upper limit: '))
+# number = 1
+# while number<upper: 
+#     print(number)
+#     number+=1
+
+# upper = int(input('Upper limit: '))
+# number = 1
+# while number<=upper: 
+#     print(number)
+#     number*=2
